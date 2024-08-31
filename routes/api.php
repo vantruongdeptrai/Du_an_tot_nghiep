11c057d82a26c321da12b30ba1c2a4b4af9bf6fb
phamthilam <ptlam120204@gmail.com>
2024-08-31 16:08:12 +0700

    sửa update category

--- routes/api.php
+++ routes/api.php
@@ -26,5 +26,5 @@ Route::delete('/tags/{id}', [TagController::class, 'destroy']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
-Route::put('/categorie/{id}', [CategoryController::class, 'update']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
