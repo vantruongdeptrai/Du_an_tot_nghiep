f0222effeaf13bd8963cd3c0e0781356487ac878
phamthilam <ptlam120204@gmail.com>
2024-09-04 23:24:33 +0700

    get category with id

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,7 @@ Route::delete('/tags/{id}', [TagController::class, 'destroy']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
 Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
