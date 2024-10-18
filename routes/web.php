15d4b9ea9f8fe53ce84834fa674399aabd21d883
ThuytrangCode <trang20040808@gmail.com>
2024-10-18 23:41:06 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,12 @@ use App\Models\DetailVariant;
 */
 
 Route::get('/', function () {
-    $product_variants = ProductVariant::with('detailVariants')->get();
-    // foreach($product_variants as $item){
-    //     dd($item->detailVariants);
-    // }
-    $attribute_values = AttributeValue::get()->all();
-    dd($attribute_values);
+    // $product_variants = ProductVariant::with('detailVariants')->get();
+    // // foreach($product_variants as $item){
+    // //     dd($item->detailVariants);
+    // // }
+    // $attribute_values = AttributeValue::get()->all();
+    // dd($attribute_values);
     return view('welcome');
 });
 
